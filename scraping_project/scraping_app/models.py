5f1228888bf3147e9f461bef561e93a4032439dd
Harman-Sahota <10harmansahota@gmail.com>
2023-08-16 17:09:43 -0400

    adding models

--- scraping_project/scraping_app/models.py
+++ scraping_project/scraping_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Website(models.Model):
+    url = models.URLField()
+
+class Keyword(models.Model):
+    word = models.CharField(max_length=100)
+
+class SearchResult(models.Model):
+    website = models.ForeignKey(Website, on_delete= models.CASCADE)
+    keyword = models.ForeignKey(Keyword, on_delete= models.CASCADE)
+    keyword_found = models.BooleanField(default=False)
+    relative_info = models.TextField(blank=True)
+
